24f1de47a207890325c42d441efa1d07ba368584
Jonas Gesch <s0564572@htw-berlin.de>
2020-05-24 19:01:40 +0200

    created components for 3 pages and added routing to navigate between them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { StatisticsPageComponent } from './components/statistics-page/statistics-page.component';
+import { InfoPageComponent } from './components/info-page/info-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'statistics', component: StatisticsPageComponent},
+  {path: 'info', component: InfoPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WalkingSkeletonComponent } from './components/walking-skeleton/walking-skeleton.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { StatisticsPageComponent } from './components/statistics-page/statistics-page.component';
+import { InfoPageComponent } from './components/info-page/info-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WalkingSkeletonComponent
+    WalkingSkeletonComponent,
+    HomePageComponent,
+    StatisticsPageComponent,
+    InfoPageComponent
   ],
   imports: [
     BrowserModule,
