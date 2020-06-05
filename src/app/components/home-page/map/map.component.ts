d812dc050018f811ab9b7a7ecd34078a98b02530
Jonas Gesch <s0564572@htw-berlin.de>
2020-06-05 16:39:35 +0200

    added more information to map markers and more map marker icons

--- src/app/components/home-page/map/map.component.ts
+++ src/app/components/home-page/map/map.component.ts
@@ -16,44 +16,55 @@ export class MapComponent implements AfterViewInit {
 
   lastTwoWeeks = [];
   trafficData = [];
-  dataFrom = "2010-06-01"
+  dataFrom = "2019-12-01"
   dataTo = "2020-06-04"
 
   markers = L.markerClusterGroup({
     spiderfyOnMaxZoom: true,
     showCoverageOnHover: false,
     zoomToBoundsOnClick: false,
-    // iconCreateFunction: function(cluster) {
-    //   let childCount = cluster.getChildCount()
-    //   let c = ' marker-cluster-';
-    //   if (childCount <= 10) {
-    //     c += 'small';
-    //   } else if (childCount <= 100) {
-    //     c += 'medium';
-    //   } else {
-    //     c += 'large';
-    //   }
+  //   iconCreateFunction: function(cluster) {
+	// 	var childCount = cluster.getChildCount();
 
-    //   return L.divIcon({ html: '<div><span>' + cluster.getChildCount() + '<span></div>',
-    //   className: 'marker-cluster ' + c,
-    //   iconSize: new L.Point(40, 40)});
-    // }
+	// 	var c = ' marker-cluster-';
+	// 	if (childCount < 10) {
+	// 		c += 'small';
+	// 	} else if (childCount < 100) {
+	// 		c += 'medium';
+	// 	} else {
+	// 		c += 'large';
+	// 	}
+
+	// 	return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster' + c, iconSize: new L.Point(50, 50) });
+	// }
   });
 
   constructionSiteIcon = L.icon({
     iconUrl: 'assets/200px-Construction.png',
+    iconSize:     [50, 44], // size of the icon
+    iconAnchor:   [25, 22], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -24] // point from which the popup should open relative to the iconAnchor
+  });
 
-    iconSize:     [30, 30], // size of the icon
-    iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -15] // point from which the popup should open relative to the iconAnchor
+  dangerIcon = L.icon({
+    iconUrl: 'assets/200px-Danger.png',
+    iconSize:     [50, 44], // size of the icon
+    iconAnchor:   [25, 22], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -24] // point from which the popup should open relative to the iconAnchor
+  });
+
+  trafficJamIcon = L.icon({
+    iconUrl: 'assets/200px-Traffic-Jam.png',
+    iconSize:     [50, 44], // size of the icon
+    iconAnchor:   [25, 22], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -24] // point from which the popup should open relative to the iconAnchor
   });
 
   roadClosureIcon = L.icon({
     iconUrl: 'assets/200px-Closure.png',
-
-    iconSize:     [30, 30], // size of the icon
-    iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -15] // point from which the popup should open relative to the iconAnchor
+    iconSize:     [50, 50], // size of the icon
+    iconAnchor:   [25, 25], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -27] // point from which the popup should open relative to the iconAnchor
   });
 
   constructor(private apiService: ApiService) { 
@@ -104,20 +115,32 @@ export class MapComponent implements AfterViewInit {
   {
     this.trafficData.forEach(element => {
       if (element != null){
-        let streetName = "";
+        let popUpContent = "<p>";
+        if(element.name != null)
+          popUpContent += "<b>" + element.name + "</b>";
         if(element.streets != null)
-          streetName = element.streets[0];
-
-        let popUpContent = "<p> <b>" + element.name + "</b>" + "<br> " + streetName + "</p>";
+          popUpContent += "<b> - " + element.streets[0] + "</b>";
+        if(element.section != null)
+          popUpContent += "<br><br>" + element.section;
+        if(element.description != null)
+          popUpContent += "<br><br>" + element.description;
+        if(element.validities[0] != null)
+          popUpContent += "<br><br>Von: " + element.validities[0].timeFrom
+                        + "<br>Bis: " + element.validities[0].timeTo;
+        popUpContent += "</p>";
 
         let icon = null;
         if(element.name == "Sperrung")
           icon = this.roadClosureIcon;
-        else if(element.name == "Baustelle")
-          icon = this.constructionSiteIcon;
+        else if(element.name == "Baustelle" || element.name == "Bauarbeiten")
+          icon = this.constructionSiteIcon;        
+        else if(element.name == "Stau")
+          icon = this.trafficJamIcon;
+        else //if(element.name == "Gefahr" || element.name == "Unfall")
+          icon = this.dangerIcon;
         if(element.location != null){
           let marker = this.addMarker(element.location.coordinates[1], element.location.coordinates[0], icon);
-          marker.bindPopup(popUpContent);
+          marker.bindPopup(popUpContent, {className: "popup"});
           this.markers.addLayer(marker);
         }
       }
